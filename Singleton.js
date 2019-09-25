6c3b2e80a058272c5f96506d82969a48e32f67ee
AntonVoronezh <pstut@mail.ru>
2019-09-25 20:28:48 +0300

    Singleton

---
+++ Singleton.js
@@ -0,0 +1,28 @@
+class Counter {
+	constructor() {
+		if (typeof Counter.instance === 'object') {
+			return Counter.instance;
+		}
+		this.count = 0;
+		Counter.instance = this;
+		return this;
+	}
+
+	getCounter() {
+		return this.count;
+	}
+
+	plusOne() {
+		this.count++;
+	}
+}
+
+const a = new Counter();
+const b = new Counter();
+
+a.plusOne();
+a.plusOne();
+a.plusOne();
+b.plusOne();
+
+console.log('Counter', a.getCounter(), b.getCounter());
